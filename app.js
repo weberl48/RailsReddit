f2647d64e42ce0cddaa180602385d4a79e1a9bdc
Lucas Weber <weberl48@outlook.com>
2015-12-29 20:26:25 -0500

    bootstrapin

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ angular.module('redditClone',[])
   function($scope){
     $scope.test = "Hello World!";
     $scope.posts = [
-      'test1',
-      'test2',
-      'test3',
-      'test4'
+      {title:'test1', upvotes: 5},
+      {title:'test2', upvotes: 2},
+      {title:'test2', upvotes: 2}
+
     ]
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') {return alert('Post must have title');}
+      $scope.posts.push({
+        title: $scope.title, upvotes: 0,
+        link: $scope.link
+      });
+        $scope.title = '';
+    }
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
+    $scope.decrementUpvotes = function(post){
+      post.upvotes -= 1;
+    }
   }
+
 ])
